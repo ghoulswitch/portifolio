621c2f50ca87cf705a634a416c5dd0ef8ec9cdae
ghoulswitch <die.millyturn@gmail.com>
2025-02-19 09:52:45 -0300

    criação do modal-dquintal

--- scr/javascript.js
+++ scr/javascript.js
@@ -6,11 +6,24 @@ function fecharModalLogoInfo() {
     document.getElementById('modal-logo-info').classList.remove('show');
 }
 
+function abrirModalLogoQuintal() {
+    document.getElementById('modal-logo-quintal').classList.add('show');
+}
+
+function fecharModalLogoQuintal() {
+    document.getElementById('modal-logo-quintal').classList.remove('show');
+}
+
 // para fechar o modal quando clicar fora dele
 window.onclick = function(event) { 
-    let modalLogoInfo = document.getElementById('modal-logo-info') 
-
-    if (event.target === modalLogoInfo) { 
-        fecharModalLogoInfo()
+    const modalLogoInfo = document.getElementById('modal-logo-info');
+    const modalLogoQuintal = document.getElementById('modal-logo-quintal');
+    
+    if (event.target === modalLogoInfo) {
+        fecharModalLogoInfo();
+    }
+    
+    if (event.target === modalLogoQuintal) {
+        fecharModalLogoQuintal();
     }
 }
