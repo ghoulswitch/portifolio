6950f41542520e11964347338d8d6b5d18369072
ghoulswitch <die.millyturn@gmail.com>
2025-02-23 15:37:19 -0300

    modal: scroll

--- src/script.js
+++ src/script.js
@@ -1,29 +1,18 @@
 function abrirModalLogoInfo() {
     document.getElementById('modal-logo-info').classList.add('show');
+    document.body.classList.add('no-scroll');
 }
 
 function fecharModalLogoInfo() {
     document.getElementById('modal-logo-info').classList.remove('show');
-}
-
-function abrirModalLogoQuintal() {
-    document.getElementById('modal-logo-quintal').classList.add('show');
-}
-
-function fecharModalLogoQuintal() {
-    document.getElementById('modal-logo-quintal').classList.remove('show');
+    document.body.classList.remove('no-scroll');
 }
 
 // para fechar o modal quando clicar fora dele
 window.onclick = function(event) { 
     const modalLogoInfo = document.getElementById('modal-logo-info');
-    const modalLogoQuintal = document.getElementById('modal-logo-quintal');
     
     if (event.target === modalLogoInfo) {
         fecharModalLogoInfo();
-    }
-    
-    if (event.target === modalLogoQuintal) {
-        fecharModalLogoQuintal();
     }
 }
