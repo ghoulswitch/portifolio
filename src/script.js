301fa87769f2e185da7092c81e08152a4376a9c0
ghoulswitch <die.millyturn@gmail.com>
2025-03-06 19:53:10 -0300

    + 1 projeto (netflix)

--- src/script.js
+++ src/script.js
@@ -8,11 +8,24 @@ function fecharModalLogoInfo() {
     document.body.classList.remove('no-scroll');
 }
 
+function abrirModalNetflix() {
+    document.getElementById('modal-netflix').classList.add('show');
+    document.body.classList.add('no-scroll');
+}
+
+function fecharModalNetflix() {
+    document.getElementById('modal-netflix').classList.remove('show');
+    document.body.classList.remove('no-scroll');
+}
+
 // para fechar o modal quando clicar fora dele
 window.onclick = function(event) { 
     const modalLogoInfo = document.getElementById('modal-logo-info');
+    const modalNetflix = document.getElementById('modal-netflix');
     
     if (event.target === modalLogoInfo) {
         fecharModalLogoInfo();
+    } if (event.target === modalNetflix) {
+        fecharModalNetflix();
     }
 }
